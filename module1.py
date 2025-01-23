4193263431cfea1846c6bf3a47f91472162ab370
opilane <opilane@HP-CZC2349HT4>
2025-01-23 13:52:09 +0200

    tund

---
+++ module1.py
@@ -0,0 +1,26 @@
+#4
+print("Kui suur on puu ümbermõõt?") 
+
+#3
+from random import randint
+
+
+kommide_arv=randint(10,100)
+print(f"Laua peal on {kommide_arv}")
+mitu=int(input("Mitu tahad süüja?"))
+print(f"Laua peal on jäänud {kommide_arv-mitu}")
+#2
+vanus=18
+eesnimi = "Jakk"
+pikkus = 16.5
+kas_käib_koolis = True
+print(f"Muutuja {vanus} on {type(vanus)}")
+print(f"Muutuja {eesnimi} on {type(eesnimi)}")
+print(f"Muutuja {pikkus} on {type(pikkus)}")
+print(f"Muutuja {kas_käib_koolis} on {type(kas_käib_koolis)}")
+#1
+print("Tere maailm! ")
+nimi=input("Mis sinu nimi on? ")
+print(f"Tere maailm!, Tervitan sind {nimi}")
+vanus=int(input("Kui vana sa oled?"))
+print(f"Tere maailm!, Tervitan sind {nimi}! sa oled {vanus} aastat vana.")
